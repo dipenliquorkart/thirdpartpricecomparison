985e8a44dced9d663134e0c86581d2ecc64f2ea5
dipenliquorkart <97153801+dipenliquorkart@users.noreply.github.com>
2022-05-24 11:42:25 +0530

    Add files via upload

--- third party.py
+++ third party.py
@@ -1248,5 +1248,47 @@ df =df[['Product Name','Liquorkart_catch_price', 'Boozbud_Catch_price',
 'Don_Vassie_Decanters_MyDeal_price', 'Sippify_mydeal_price']]
 
 df.to_csv('C:\\Users\\dipen\\third_party_prices.csv')
+from datetime import date
+date = date.today()
+d4 = date.strftime("%b-%d")
 
+df = pd.read_csv('third_party_prices.csv',index_col=0)
+
+
+lqprice = pd.read_csv('products.csv',index_col=0)
+
+lqprice = lqprice[['Title','Cost per item','Variant Inventory Qty']]
+
+df = df.join(lqprice.set_index('Title'), on='Product Name')     
+
+
+df.columns = ['Product Name', 'Liquorkart_catch_price', 'Boozbud_Catch_price',
+       'Booze_House_catch_price', 'hello_drink_catch_price',
+       'myliquoronline_catch_price', 'mr_Danks_Liquor_Catch_price',
+       'PAULS_LIQUOR_STORE_PTY_LTD_Catch_price', 'Liquor_Geeks_Catch_price',
+       'Super_Liquor_Store_Catch_price', 'The_Gin_Boutique_Catch_price',
+       'Secret_Bottle_Australia_Catch_price', 'Cocktail_Kit_Catch_price',
+       'Carlton_United_Breweries_Catch_price', 'BulkPantry_Catch_price',
+      'Wine_Relique_Catch_price',
+       'Booze_Barrels_Catch_price', 'Sippify_Catch_price',
+       'GoodDrop_Catch_price', 'Drinks_Network_Catch_price',
+       'Liquorkart_Mydeal_price', 'boozbud_mydeal_price',
+       'hello_drink_mydeal_price',
+       'liquor_loot_mydeal_price', 'Timex_Mydeal_price',
+       'Mr_Danks_Liquor_MyDeal_price',
+       'Secret_Bottle_MyDeal_price', 'CocktailKit_Mydeal_price',
+       'Carton_United_Breweries_Mydeal_price',
+       'Don_Vassie_Decanters_MyDeal_price', 'Sippify_mydeal_price',
+       'liquorkart_cost_price','Variant Inventory Qty']
+
+
+
+
+#%%
+
+
+
+
+df.to_csv(f"third_party_Price_comparison-{d4}.csv")
+print('File Created')
 
